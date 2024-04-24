286f7cbd9d397b64515df67f00449cff3b6f3e5c
Swartline <77667035+Swartline@users.noreply.github.com>
2024-04-24 14:14:12 +0200

    add inventory function

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@
 import { bootstrapExtra } from '@workadventure/scripting-api-extra';
 import { addItem, getItems, removeItem, setItems } from './item/functions';
 import { Item } from './item/item';
+import { addInInventory, initInventory, getInventory, removeInventory } from "./inventory/functions";
 import itemsJson from './item/items.json';
 
 const items: Array<Item> = itemsJson.items;
@@ -10,15 +11,8 @@ const items: Array<Item> = itemsJson.items;
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
+    initInventory()
     bootstrapExtra().catch((e) => console.error(e));
 
-    // TODO remove this
-    setItems(items);
-
-    removeItem(items[0]);
-    console.log(getItems());
-
-    addItem(items[0]);
-    console.log(getItems());
   })
   .catch((e) => console.error(e));
