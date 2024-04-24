4e5c4fe25aedce7b6155a4a8e0317d941622c6e0
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-04-24 15:11:49 +0200

    Use custom library

--- src/inventories/functions.ts
+++ src/inventories/functions.ts
@@ -1,48 +1,51 @@
-import { Item } from '../items/item';
+import { Item } from 'workadventure-inventory';
 
 export const getInventory = (): Array<Item> => {
-    const inventory: Array<Item> = WA.player.state.loadVariable("inventory") as Array<Item>
+  const inventory: Array<Item> = WA.player.state.loadVariable(
+    'inventory',
+  ) as Array<Item>;
 
-    function compareByName(a: Item, b: Item) {
-        const nameA = a.name.toUpperCase();
-        const nameB = b.name.toUpperCase();
-    
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-            return 1;
-        }
-        return 0;
+  function compareByName(a: Item, b: Item) {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
     }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  }
 
-    inventory.sort(compareByName)
+  inventory.sort(compareByName);
 
-    return inventory
+  return inventory;
 };
 
-export const initInventory = () =>{
-    if (getInventory() === undefined) {
-        WA.player.state.saveVariable("inventory", []);
-    }
+export const initInventory = () => {
+  if (getInventory() === undefined) {
+    WA.player.state.saveVariable('inventory', []);
+  }
 };
 
 export const addInInventory = (item: Item) => {
-    const playerInventory = getInventory()
-    playerInventory.push(item)
-    WA.player.state.saveVariable("inventory", playerInventory);
+  const playerInventory = getInventory();
+  playerInventory.push(item);
+  WA.player.state.saveVariable('inventory', playerInventory);
 
-    return playerInventory
+  return playerInventory;
 };
 
 export const removeInventory = (item_id: number) => {
-    const playerInventory = getInventory()
-    playerInventory.forEach((currentItem: Item, index: number) => {
-        if (currentItem.id === item_id) {
-            playerInventory.splice(index, 1 )
-        }}
-    );
-    WA.player.state.saveVariable("inventory", playerInventory);
+  const playerInventory = getInventory();
+  playerInventory.forEach((currentItem: Item, index: number) => {
+    if (currentItem.id === item_id) {
+      playerInventory.splice(index, 1);
+    }
+  });
+  WA.player.state.saveVariable('inventory', playerInventory);
 
-    return playerInventory;
-  };
+  return playerInventory;
+};
+
