9a505fe0569ebc6333b4541c1aafd340544cfedb
Swartline <77667035+Swartline@users.noreply.github.com>
2024-04-24 14:14:12 +0200

    add inventory function

---
+++ src/inventory/functions.ts
@@ -0,0 +1,48 @@
+import { Item } from '../item/item';
+
+export const getInventory = (): Array<Item> => {
+    const inventory: Array<Item> = WA.player.state.loadVariable("inventory") as Array<Item>
+
+    function compareByName(a: Item, b: Item) {
+        const nameA = a.name.toUpperCase();
+        const nameB = b.name.toUpperCase();
+    
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    }
+
+    inventory.sort(compareByName)
+
+    return inventory
+};
+
+export const initInventory = () =>{
+    if (getInventory() === undefined) {
+        WA.player.state.saveVariable("inventory", []);
+    }
+};
+
+export const addInInventory = (item: Item) => {
+    const playerInventory = getInventory()
+    playerInventory.push(item)
+    WA.player.state.saveVariable("inventory", playerInventory);
+
+    return playerInventory
+};
+
+export const removeInventory = (item_id: number) => {
+    const playerInventory = getInventory()
+    playerInventory.forEach((currentItem: Item, index: number) => {
+        if (currentItem.id === item_id) {
+            playerInventory.splice(index, 1 )
+        }}
+    );
+    WA.player.state.saveVariable("inventory", playerInventory);
+
+    return playerInventory;
+  };
