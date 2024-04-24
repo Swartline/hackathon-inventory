ef1dba026e816ebcad0ae3b721085ee121b8e659
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2024-04-24 14:55:32 +0200

    Remove unused imports

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from '@workadventure/scripting-api-extra';
-import { addItem, getItems, removeItem, setItems } from './items/functions';
-import { Item } from './items/item';
-import { addInInventory, initInventory, getInventory, removeInventory } from "./inventories/functions";
-import itemsJson from './items/items.json';
-
-const items: Array<Item> = itemsJson.items;
+import { initInventory } from './inventories/functions';
 
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
-    initInventory()
+    initInventory();
     bootstrapExtra().catch((e) => console.error(e));
-
   })
   .catch((e) => console.error(e));
