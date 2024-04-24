e59e5b4fa1096ed69ade18f94249154a21976e72
Swartline <77667035+Swartline@users.noreply.github.com>
2024-04-24 14:14:12 +0200

    add inventory function

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from '@workadventure/scripting-api-extra';
-import { addItem, getItems, removeItem, setItems } from './items/functions';
-import { Item } from './items/item';
-import itemsJson from './items/items.json';
+import { addItem, getItems, removeItem, setItems } from './item/functions';
+import { Item } from './item/item';
+import { addInInventory, initInventory, getInventory, removeInventory } from "./inventory/functions";
+import itemsJson from './item/items.json';
 
 const items: Array<Item> = itemsJson.items;
 
 // Waiting for the API to be ready
 WA.onInit()
   .then(() => {
+    initInventory()
     bootstrapExtra().catch((e) => console.error(e));
 
-    // TODO remove this
-    setItems(items);
-
-    removeItem(items[0]);
-    console.log(getItems());
-
-    addItem(items[0]);
-    console.log(getItems());
   })
   .catch((e) => console.error(e));
