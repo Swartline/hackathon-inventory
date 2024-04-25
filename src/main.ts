ea85671bf6004bf285304d41fb1d52fa4c65d0a3
fred <frederic.lopez9@hotmail.com>
2024-04-25 11:34:23 +0200

    fix: inventory feature

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { UIWebsite } from "@workadventure/iframe-api-typings";
 import {
   Item,
   addPlayerItem,
+  clearPlayerInventory,
   initPlayerInventory,
 } from "workadventure-inventory";
 import itemsJson from "./items/items.json";
@@ -40,41 +41,42 @@ WA.onInit()
 
     let inventoryIframe: UIWebsite | undefined;
 
-    WA.ui.actionBar.addButton({
-      id: "inventory-btn",
-      type: "action",
-      imageSrc: "https://cdn-icons-png.flaticon.com/512/4138/4138061.png",
-      toolTip: "Inventaire",
-      callback: async (event) => {
-        console.log("Button clicked", event);
-        if (!inventoryIframe) {
-          inventoryIframe = await WA.ui.website.open({
-            url: "../test.html",
-            position: {
-              vertical: "middle",
-              horizontal: "middle",
-            },
-            size: {
-              height: "50vh",
-              width: "50vw",
-            },
-            allowApi: true,
-          });
-          inventoryIframe.position.vertical = "top";
+  let inventoryIframe: UIWebsite | undefined;
 
-          WA.player.state.inventory_open = true;
-        } else {
-          inventoryIframe.close();
-          inventoryIframe = undefined;
-          WA.player.state.inventory_open = false;
-        }
-      },
-    });
+  WA.ui.actionBar.addButton({
+    id: "inventory-btn",
+    type: "action",
+    imageSrc: "https://cdn-icons-png.flaticon.com/512/4138/4138061.png",
+    toolTip: "Inventaire",
+    callback: async (event) => {
+      if (!inventoryIframe) {
+        inventoryIframe = await WA.ui.website.open({
+          url: "../test.html",
+          position: {
+            vertical: "middle",
+            horizontal: "middle",
+          },
+          size: {
+            height: "50vh",
+            width: "50vw",
+          },
+          allowApi: true,
+        });
+        inventoryIframe.position.vertical = "top";
 
-    WA.player.state.onVariableChange("inventory_open").subscribe((value) => {
-      if (!value) {
+        WA.player.state.inventory_open = true;
+        WA.player.state.inventory_id = inventoryIframe.id;
+      } else {
+        inventoryIframe.close();
         inventoryIframe = undefined;
+        WA.player.state.inventory_open = false;
       }
-    });
-  })
-  .catch((e) => console.error(e));
+    },
+  });
+
+  WA.player.state.onVariableChange("inventory_open").subscribe((value) => {
+    if (!value) {
+      inventoryIframe = undefined;
+    }
+  });
+})();
