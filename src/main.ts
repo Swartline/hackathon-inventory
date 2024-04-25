7083857b8abd49108de5d09ff2db9c5cee33a0e7
fred <frederic.lopez9@hotmail.com>
2024-04-25 14:59:30 +0200

    setup for lib

--- src/main.ts
+++ src/main.ts
@@ -1,63 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { UIWebsite } from "@workadventure/iframe-api-typings";
-import {
-  Item,
-  addPlayerItem,
-  clearPlayerInventory,
-  initPlayerInventory,
-} from "workadventure-inventory";
-import itemsJson from "./items/items.json";
-
-const items: Item[] = itemsJson.items;
+import { initializeInventorySystem } from "workadventure-inventory";
 
 // Waiting for the API to be ready
 (async () => {
   await WA.onInit();
-
-  await initPlayerInventory();
-
-  await clearPlayerInventory();
-  for (const item of items) {
-    await addPlayerItem(item);
-  }
-
-  let inventoryIframe: UIWebsite | undefined;
-
-  WA.ui.actionBar.addButton({
-    id: "inventory-btn",
-    type: "action",
-    imageSrc: "https://cdn-icons-png.flaticon.com/512/4138/4138061.png",
-    toolTip: "Inventaire",
-    callback: async () => {
-      if (!inventoryIframe) {
-        inventoryIframe = await WA.ui.website.open({
-          url: "/src/test.html",
-          position: {
-            vertical: "middle",
-            horizontal: "middle",
-          },
-          size: {
-            height: "50vh",
-            width: "50vw",
-          },
-          allowApi: true,
-        });
-        inventoryIframe.position.vertical = "top";
-
-        WA.player.state.inventory_open = true;
-        WA.player.state.inventory_id = inventoryIframe.id;
-      } else {
-        inventoryIframe.close();
-        inventoryIframe = undefined;
-        WA.player.state.inventory_open = false;
-      }
-    },
-  });
-
-  WA.player.state.onVariableChange("inventory_open").subscribe((value) => {
-    if (!value) {
-      inventoryIframe = undefined;
-    }
-  });
+  await initializeInventorySystem();
 })();
